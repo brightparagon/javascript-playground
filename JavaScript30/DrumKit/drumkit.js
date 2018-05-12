4411be2ed636c21b13f254196078c836ee44a756
노경모 <kyeongmo2@gmail.com>
2018-05-12 16:37:54 +0900

    JavaScript30: Day1: create Drum Kit

---
+++ JavaScript30/DrumKit/drumkit.js
@@ -0,0 +1,17 @@
+function playSound(event) {
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+  if (!audio) return; // stop the function if there is no corresponding audio
+  audio.currentTime = 0; // enable kicking off sound on every clicking
+  audio.play();
+  key.classList.add('playing');
+};
+
+function removeTransition(event) {
+  if (event.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
