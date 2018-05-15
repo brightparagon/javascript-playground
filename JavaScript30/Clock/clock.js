7316716d1c89bb523d3f6d6e7126937c3ad52d1a
노경모 <kyeongmo2@gmail.com>
2018-05-15 21:57:36 +0900

    JavaScript30: Clock: complete functions and design & add js documentation

--- JavaScript30/Clock/clock.js
+++ JavaScript30/Clock/clock.js
@@ -1,29 +1,54 @@
+/**
+ * @description called every second and rotates hands
+ * @param {object} counter - hold time variable
+ */
 function tick(counter) {
-  const sec = document.querySelector('.clock .sec');
+  const sec = getHand('.clock .sec');
   const min = getHand('.clock .min');
   const hour = getHand('.clock .hour');
 
-  sec.style.transform = `rotate(${counter.getCount()}deg)`;
+  rotateHand(sec, counter.getCount() * 360 / 60 - 90);
+  rotateHand(min, Math.trunc(counter.getCount() / 60) * 360 / 60 - 90);
+  rotateHand(hour, Math.trunc(counter.getCount() / (60 * 60)) * 360 / 12 - 90);
 
-  counter.increase(30);
+  counter.increase();
 }
 
+/**
+ * @description get a hand element by a selector
+ * @param {string} selector - css query selector
+ * @returns found html element by the selector
+ */
 function getHand(selector) {
   return document.querySelector(selector);
 }
 
+/**
+ * @description rotate an element to a given degree
+ * @param {object} element 
+ * @param {number} degree 
+ */
+function rotateHand(element, degree) {
+  element.style.transform = `rotate(${degree}deg)`;
+}
+
+/**
+ * @description create a counter holding time variable
+ * @returns an object with two methods
+ */
 function createCounter() {
   let count = 0;
 
   return {
-    getCount: function() {
+    getCount: () => {
       return count; 
     },
-    increase: function(deg) {
-      count += deg;
+    increase: () => {
+      count++;
     }
   };
 }
 
+// create a counter and call tick every second
 const counter = createCounter();
 setInterval(() => {tick(counter)}, 1000);
